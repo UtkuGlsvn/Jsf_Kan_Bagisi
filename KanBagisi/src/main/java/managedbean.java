3954bf71dc66cd9672ae15f81e3e4b3f260cd42c
utkuglsvn <utku.glsvn@gmail.com>
2018-04-03 23:58:47 +0300

    Add files via upload

---
+++ KanBagisi/src/main/java/managedbean.java
@@ -0,0 +1,22 @@
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+@ManagedBean
+@RequestScoped
+public class managedbean  {
+      public String bagisciyim_git()
+      {
+           return "bagisciyim?faces-redirect=true";
+      }
+      public String arama_form_git()
+      {
+           return "arama_formu?faces-redirect=true";
+      }
+        public String bagis_git()
+      {
+           return "bagis?faces-redirect=true";
+      }
+              public String aranan_git()
+      {
+           return "aranan?faces-redirect=true";
+      }
+}
